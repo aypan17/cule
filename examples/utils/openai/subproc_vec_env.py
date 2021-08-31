d883e35f4e4e208b4140d54c0eed7ac570fd2bec
aypan17 <aypan.17@gmail.com>
2021-08-30 21:51:15 -0700

    render in viz fixed

--- examples/utils/openai/subproc_vec_env.py
+++ examples/utils/openai/subproc_vec_env.py
@@ -12,7 +12,8 @@ def tile_images(img_nhwc):
         bigim_HWc, ndarray with ndim=3
     """
     img_nhwc = np.asarray(img_nhwc)
-    N, h, w, c = img_nhwc.shape
+    N = 1
+    h, w, c = img_nhwc.shape
     H = int(np.ceil(np.sqrt(N)))
     W = int(np.ceil(float(N)/H))
     img_nhwc = np.array(list(img_nhwc) + [img_nhwc[0]*0 for _ in range(N, H*W)])
